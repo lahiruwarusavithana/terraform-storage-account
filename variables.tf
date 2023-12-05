4866e3a47d9c8c4d768dd5bf729c5c94e2c1367c
Lahiru Warusavithana <lahiru.warusavithana@ifs.com>
2023-12-05 14:57:29 +0530

    Updated

---
+++ variables.tf
@@ -0,0 +1,22 @@
+
+variable "resource_group_name" {
+
+  description = "Name of the resource group"
+  type = string
+
+}
+
+variable "location" {
+
+  description = "Name of the Cloud region"
+  type = string
+
+}
+
+variable "storage_account_name" {
+
+  description = "Name of the storage Account"
+  type = string
+
+}
+
